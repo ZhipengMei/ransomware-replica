4e8bc94c8f00bf2ce28d0e9abc7d63dce27b5c05
AnimeMei <adrian.z.mei@gmail.com>
2017-12-02 22:09:17 -0800

    modified server.js

--- 4_API/rsa_Api/server.js
+++ 4_API/rsa_Api/server.js
@@ -1,29 +1,36 @@
-var express = require('express'),
-  app = express(),
-  port = process.env.PORT || 3000,
-  mongoose = require('mongoose'),
-  Task = require('./api/models/rsaModel'), //created model loading here
-  bodyParser = require('body-parser');
-
-// mongoose instance connection url connection
-mongoose.Promise = global.Promise;
+// var express = require('express'),
+//   app = express(),
+//   port = process.env.PORT || 3000,
+//   mongoose = require('mongoose'),
+//   Task = require('./api/models/rsaModel'), //created model loading here
+//   bodyParser = require('body-parser');
+//
+// // mongoose instance connection url connection
+// mongoose.Promise = global.Promise;
 // mongoose.connect('mongodb://localhost/rsadb');
-mongoose.connect('http://ec2-13-58-22-230.us-east-2.compute.amazonaws.com:3000/rsadb');
+// // mongoose.connect('http://ec2-13-58-22-230.us-east-2.compute.amazonaws.com:3000/rsadb');
+//
+//
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+//
+// // //middleware code to intercepts incoming http request
+// // app.use(function(req, res) {
+// //   res.status(404).send({url: req.originalUrl + ' not found'})
+// // });
+//
+// var routes = require('./api/routes/rsaRoutes'); //importing route
+// routes(app); //register the route
+//
+//
+// app.listen(port);
+//
+//
+// console.log('rsa RESTful API server started on: ' + port);
 
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-// //middleware code to intercepts incoming http request
-// app.use(function(req, res) {
-//   res.status(404).send({url: req.originalUrl + ' not found'})
-// });
-
-var routes = require('./api/routes/rsaRoutes'); //importing route
-routes(app); //register the route
-
-
-app.listen(port);
-
-
-console.log('rsa RESTful API server started on: ' + port);
+const express = require('express')
+const app = express()
+app.get('/', (req, res) => {
+  res.send('HEY!')
+})
+app.listen(3000, () => console.log('Server running on port 3000'))
