8eba707061e1dd9c37920fb0df7a940d832bf898
AnimeMei <adrian.z.mei@gmail.com>
2017-12-02 22:19:34 -0800

    modified server.js

--- 4_API/rsa_Api/server.js
+++ 4_API/rsa_Api/server.js
@@ -1,6 +1,7 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000,
+  // port = process.env.PORT || 3000,
+  port = process.env.PORT || 80,
   mongoose = require('mongoose'),
   Task = require('./api/models/rsaModel'), //created model loading here
   bodyParser = require('body-parser');
